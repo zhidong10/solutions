f91b470a26d7ace04be9e9b7edd8badd17c9aebd
zhidong <zhidong10@foxmail.com>
2013-11-03 18:20:35 +0800

    三个脚本效果
    进度条、焦点图、推进式立体展示图。

---
+++ Spacegallery/js/layout.js
@@ -0,0 +1,28 @@
+(function($){
+	var initLayout = function() {
+		var hash = window.location.hash.replace('#', '');
+		var currentTab = $('ul.navigationTabs a')
+							.bind('click', showTab)
+							.filter('a[rel=' + hash + ']');
+		if (currentTab.size() == 0) {
+			currentTab = $('ul.navigationTabs a:first');
+		}
+		showTab.apply(currentTab.get(0));
+		$('#myGallery').spacegallery({loadingClass: 'loading'});
+	};
+	
+	var showTab = function(e) {
+		var tabIndex = $('ul.navigationTabs a')
+							.removeClass('active')
+							.index(this);
+		$(this)
+			.addClass('active')
+			.blur();
+		$('div.tab')
+			.hide()
+				.eq(tabIndex)
+				.show();
+	};
+	
+	EYE.register(initLayout, 'init');
+})(jQuery)
---
+++ myFocus/说明.txt
@@ -0,0 +1,2 @@
+使用说明：http://www.cosmissy.com/myfocus/tutorials.html
+使用API：http://www.cosmissy.com/myfocus/api.html
